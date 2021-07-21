866f7e1d82c950bf416a57b701a209ca42c2546c
benjoboy <serec.benjamin@gmail.com>
2021-07-21 16:23:56 +0200

    getschedule endpoint

--- models/familyModel.js
+++ models/familyModel.js
@@ -1,12 +1,28 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+const ScheduleItemSchema = new Schema({
+  title: String,
+  description: String,
+  start: Date,
+  end: Date,
+  isAllDay: false,
+});
+
 var familySchema = new Schema({
   owner: Schema.Types.ObjectId,
   name: String,
   members: [],
   invites: [],
+  schedule: [ScheduleItemSchema],
 });
 
+var ScheduleItem = mongoose.model("ScheduleItem", ScheduleItemSchema);
 var Family = mongoose.model("Family", familySchema);
-module.exports = Family;
+module.exports = { Family, ScheduleItem };
+
+//authorize user as member of family
+/*
+familySchema.statics.authorize = function () {
+
+}*/
